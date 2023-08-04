0eff9f9ca8fdabb2b59317b22611b3041188af46
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-08-03 20:48:11 -0400

    Output Management webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Img from './img.png';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
@@ -14,6 +16,11 @@ function component() {
 
   element.appendChild(myImage);
 
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+  
   return element;
 }
 
