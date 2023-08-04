d31dd5e44d3912fc8c16406b67829c15bf2738c3
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-08-03 21:18:10 -0400

    Improve style

--- src/index.js
+++ src/index.js
@@ -6,20 +6,18 @@ import printMe from './print.js';
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
   const myImage = new Image();
-  myImage.src = Img;
 
-  element.appendChild(myImage);
+  element.innerHTML = _.join(['-->', ''], ' ');
+  element.classList.add('template');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
+  myImage.src = Img;
+
   element.appendChild(btn);
+  element.appendChild(myImage);
   
   return element;
 }
